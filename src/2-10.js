d3d381dd2ce5f256fa40e622c3e71c471cd11ee2
Clem99AJ <924770334@qq.com>
2022-08-05 16:57:06 +0800

    2-10提交

---
+++ src/2-10.js
@@ -0,0 +1,20 @@
+
+//在ES5中，定义对象的属性是，对象外的一个变量
+// let name = '楠溪泽岸'
+// let age = 21
+// let obj = {
+//     name:name,
+//     age:age
+// }
+// console.log(obj)
+
+//在ES6在简化后，如果对象属性名与对象外的变量同名，则可以只写键
+// let name = '楠溪泽岸'
+// let age = 21
+// let obj = {
+//     name,
+//     age
+// }
+// console.log(obj)
+
+
